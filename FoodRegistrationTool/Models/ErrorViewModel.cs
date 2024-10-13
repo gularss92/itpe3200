a9f540a4c0b42bfba86bbc800c2bd6dd5ed5d97b
gularss92 <gunnar.larsson92@gmail.com>
2024-10-13 17:05:30 +0200

    Lagt til MVC-rammeverk

---
+++ FoodRegistrationTool/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FoodRegistrationTool.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
