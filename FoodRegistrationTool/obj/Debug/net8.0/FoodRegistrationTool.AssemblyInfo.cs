dbdaa8e27ccf419be5c434285aa4223b81bbbdbe
Kjell Wiggo Abrahamsen <s374924@oslomet.no>
2024-11-09 16:02:22 +0100

    Added Producer Partial View
    Added _ProducerTable partial view.

--- FoodRegistrationTool/obj/Debug/net8.0/FoodRegistrationTool.AssemblyInfo.cs
+++ FoodRegistrationTool/obj/Debug/net8.0/FoodRegistrationTool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodRegistrationTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678af791b96426ee3e81d95b7aca4ab0dce8e0d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96bd45dd14084d1762329838d9e30244472ff3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodRegistrationTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodRegistrationTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
