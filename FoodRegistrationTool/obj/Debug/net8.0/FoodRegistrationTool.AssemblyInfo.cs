963a7aaa81464545691fbe7e88704e18a2da37d1
Kjell Wiggo Abrahamsen <s374924@oslomet.no>
2024-11-10 18:47:44 +0100

    View and button changes

--- FoodRegistrationTool/obj/Debug/net8.0/FoodRegistrationTool.AssemblyInfo.cs
+++ FoodRegistrationTool/obj/Debug/net8.0/FoodRegistrationTool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodRegistrationTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98267afbab0ca11ca874a05c0d555cf28761105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682a01cec8c7cb38fafc87fcbfbdaeb390dc8109")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodRegistrationTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodRegistrationTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
