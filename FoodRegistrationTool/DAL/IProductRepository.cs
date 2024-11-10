06bcf4d48c45633e9c19b5b271544a4fcc77dbe7
Kjell Wiggo Abrahamsen <s374924@oslomet.no>
2024-11-10 19:57:21 +0100

    Fixed Interface implementation

--- FoodRegistrationTool/DAL/IProductRepository.cs
+++ FoodRegistrationTool/DAL/IProductRepository.cs
@@ -9,7 +9,11 @@ public interface IProductRepository
     Task<bool> Create(Product product);
     Task<bool> Update(Product product);
     Task<bool> Delete(int id);
+
+    // Producer CRUD
     Task<IEnumerable<Producer>> GetAllProducers();
     Task<Producer?> GetProducerById(int id);
-    Task CreateProducer(Producer producer);
+    Task<bool> CreateProducer(Producer producer);
+    Task<bool> UpdateProducer(Producer producer);
+    Task<bool> DeleteProducer(int id);
 }
