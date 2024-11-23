f85bb1aec84856d6509534599c0ba1bf83514f9b
Kjell Wiggo Abrahamsen <s374924@oslomet.no>
2024-11-23 23:16:25 +0100

    Added Test for Table method

--- FoodRegistrationTool.Tests/Controllers/ProducerControllerTests.cs
+++ FoodRegistrationTool.Tests/Controllers/ProducerControllerTests.cs
@@ -1,16 +1,39 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Moq;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
 using FoodRegistrationTool.Controllers;
-using FoodRegistrationTool.DAL;
 using FoodRegistrationTool.Models;
-using FoodRegistrationTool.ViewModels;
+using FoodRegistrationTool.DAL;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace FoodRegistrationTool.Test.Controllers;
 public class ProducerControllerTests
 {
-    public async Task TestTable()
+    [Fact]
+    public async Task Table_ReturnsViewResult_WithListOfProducers()
     {
         // Arrange
+        var mockProductRepository = new Mock<IProductRepository>();
+        var mockLogger = new Mock<ILogger<ProducerController>>();
+        var producerController = new ProducerController(mockProductRepository.Object, mockLogger.Object);
+
+        var producers = new List<Producer>
+        {
+            new Producer { ProducerId = 1, Name = "Producer 1", Address = "Test Road"},
+            new Producer { ProducerId = 2, Name = "Producer 2", Address = "Test Avenue"}
+        };
+        mockProductRepository.Setup(repo => repo.GetAllProducers()).ReturnsAsync(producers);
+
+        // Act
+        var result = await producerController.Table();
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<IEnumerable<Producer>>(viewResult.Model);
+        Assert.Equal(2, model.Count());
     }
 }
